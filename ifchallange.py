571c73aeef9b0e8f07f88250002f6a0bfdfe4fe7
bigburd97 <noah.w.murray2015@gmail.com>
2023-08-17 22:49:18 +0000

    just the important ones

---
+++ ifchallange.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+benders = ["air" , "water" , "fire" , "earth"]
+element = input("what ellement do you control?: ")
+if element == "water":
+    print("we have to get to the northern temple!")
+elif element == "fire":
+    print("Protection and power are overrated, I think you’re very wise to choose happiness and love. – Uncle Iroh")
+elif element == "air":
+    print("yip! yip!")
+elif element == "earth":
+    print("GET OVER HERE TWINKLE TOES.")
+else:
+    print("ah another commoner.")
